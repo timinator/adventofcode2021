db40d2c937f1724e6c3a2f73ac0ec8588e66979b
Tim <1553926+timinator@users.noreply.github.com>
2021-12-15 11:02:53 +0200

    Passing test for window increase function

--- day1/puzzle.py
+++ day1/puzzle.py
@@ -1,6 +1,7 @@
 class Puzzle:
     def __init__(self, puzzle_input):
-        self.puzzle_input = puzzle_input
+        # Don't like doing work in the constructor, but I'm lazy
+        self.puzzle_input = [int(val) for val in puzzle_input]
 
     def print_puzzle_input(self):
         print(f"{self.puzzle_input}")
@@ -8,6 +9,17 @@ class Puzzle:
     def calculate_increase(self):
         count = 0
         for i,value in enumerate(self.puzzle_input):
-            if len(self.puzzle_input) > (i+1) and int(self.puzzle_input[i+1]) > int(value):
+            if len(self.puzzle_input) > (i+1) and self.puzzle_input[i+1] > value:
+                count += 1
+        return count
+
+    def calculate_window_increase(self):
+        count = 0
+        for i,value in enumerate(self.puzzle_input):
+            if len(self.puzzle_input) <= (i+3):
+                return count
+            current_window = [value, self.puzzle_input[i+1], self.puzzle_input[i+2]]
+            next_window = [self.puzzle_input[i+1], self.puzzle_input[i+2], self.puzzle_input[i+3]]
+            if sum(current_window) < sum(next_window):
                 count += 1
         return count
--- day1/test_puzzle.py
+++ day1/test_puzzle.py
@@ -4,9 +4,13 @@ from puzzle import Puzzle
 
 class TestPuzzle:
     def test_puzzle_input(self):
-        p = Puzzle("171\n154\n155\n")
-        assert p.puzzle_input == "171\n154\n155\n"
+        p = Puzzle(['171','154','155'])
+        assert p.puzzle_input == [171,154,155]
     def test_calculate_increase(self):
         data = Path("day1/test_puzzle_input.txt").read_text().rstrip().split("\n")
         p = Puzzle(data)
         assert p.calculate_increase() == 7
+    def test_calculate_window_increase(self):
+        data = Path("day1/test_puzzle_input.txt").read_text().rstrip().split("\n")
+        p = Puzzle(data)
+        assert p.calculate_window_increase() == 5
