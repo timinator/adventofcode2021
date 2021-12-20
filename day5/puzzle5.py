483ed6e9286cfe4cab0839cecdefd7e0577bbf6e
Tim <tim@onoconnect.com>
2021-12-20 22:03:58 +0200

    Add line object, shifted some code around

--- day5/puzzle5.py
+++ day5/puzzle5.py
@@ -1,3 +1,33 @@
+import pdb
+
 class Puzzle5:
     def __init__(self, puzzle_input):
-        self.puzzle_input = [[y.split(',') for y in x.split(" -> ")] for x in puzzle_input.split("\n")]
+        self.puzzle_input = [Line(x) for x in puzzle_input.split("\n")]
+
+    def calculate_line_overlap(self):
+        return 0
+
+class Line:
+    def __init__(self, line_input):
+        parsed_input = [[int(y) for y in x.split(",")] for x in line_input.split(" -> ")]
+        self.coord1 = tuple(parsed_input[0])
+        self.coord2 = tuple(parsed_input[1])
+
+    # Need to neaten this up, perhaps with a collection function or two (e.g. reduce)
+    def matching_points(self):
+        if self.coord1[0] == self.coord2[0]:
+            axis = 1
+            common_value = self.coord1[0]
+        elif self.coord1[1] == self.coord2[1]:
+            axis = 0
+            common_value = self.coord1[1]
+        else:
+            return []
+
+        points = []
+        for i in range(min(self.coord1[axis], self.coord2[axis]), max(self.coord1[axis], self.coord2[axis])+1):
+            if axis == 1:
+                points.append((common_value,i))
+            else:
+                points.append((i,common_value))
+        return points
--- day5/test_puzzle5.py
+++ day5/test_puzzle5.py
@@ -1,8 +1,18 @@
 import pytest
 from pathlib import Path
-from puzzle5 import Puzzle5
+from puzzle5 import Puzzle5,Line
 
 class TestPuzzle5:
-    def test_puzzle_input(self):
-        p = Puzzle5('0,9 -> 5,9\n8,0 -> 0,8')
-        assert p.puzzle_input == [[['0','9'], ['5','9']], [['8','0'], ['0','8']]]
+    def test_line_input(self):
+        l = Line('0,9 -> 5,9')
+        assert l.coord1 == (0,9)
+        assert l.coord2 == (5,9)
+
+    def test_line_matching_points(self):
+        l = Line('0,9 -> 5,9')
+        assert l.matching_points() == [(0,9), (1,9), (2,9), (3,9), (4,9), (5,9)]
+
+    def test_calculate_line_overlap(self):
+        data = Path("day5/test_puzzle5_input.txt").read_text().rstrip()
+        p = Puzzle5(data)
+        assert p.calculate_line_overlap() == 5
